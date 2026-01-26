af312688e814d06f14c8845b51b984c4f6f01dde
Justy <justyoberg1@gmail.com>
2026-01-25 16:49:52 -0800

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on ${port}`);
+});
