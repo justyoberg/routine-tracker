45e3adab6c413ef7e30a026c4e0ad4854c25cc4a
Justy <justyoberg1@gmail.com>
2026-01-25 17:09:49 -0800

    Fixed some file structures

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello world!');
-});
-
 app.listen(port, () => {
-  console.log(`Example app listening on ${port}`);
+  console.log(`Listening on ${port}`);
 });
